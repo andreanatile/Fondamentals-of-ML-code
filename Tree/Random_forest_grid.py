605d4789eae72cdb4297706e58aa24617d60780c
andreanatile <andrea.natile.an@gmail.com>
2023-12-29 19:34:18 +0100

    random forest grid

--- Tree/Random_forest_grid.py
+++ Tree/Random_forest_grid.py
@@ -1,10 +1,10 @@
 import numpy as np
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier,plot_tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import plot_tree
+from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
 from sklearn.model_selection import train_test_split,GridSearchCV
-from sklearn.metrics import accuracy_score,classification_report,confusion_matrix
-import matplotlib.pyplot as plt 
-np.random.seed(42)
+import matplotlib.pyplot as plt
 
 # Load the dataset
 diabetes=pd.read_csv('.\dataset\diabetes.csv')
@@ -16,31 +16,23 @@ y = diabetes['Outcome'].values
 # Split the dataset into training and test sets through hold-out strategy
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.2, stratify=y)
 
-
-# Set grid search
 param_grid={
-    'criterion':['gini','entropy'],
-    'max_depth':range(4,11),
-    'min_samples_leaf': range(1,6)
+    'n_estimators':list(range(10,51,10)),
+    'criterion': ['entropy','gini'],
+    'max_depth':list(range(5,21,5)),
+    'min_samples_leaf':list(range(2,5,1))
 }
 
-# Create the grid object
-grid=GridSearchCV(DecisionTreeClassifier(random_state=42),param_grid,scoring='accuracy',cv=5)
-
-# Train all the classifiers
+grid=GridSearchCV(RandomForestClassifier(random_state=42),param_grid,cv=5,scoring='accuracy')
 grid.fit(X_train,y_train)
 
-# Get the best model and hyperparameters
 best_model=grid.best_estimator_
+best_params=grid.best_params_
+
 y_pred=best_model.predict(X_test)
 
 #Evaluation of the model
 print(f"Accuracy TEST: {accuracy_score(y_test, y_pred)}")
 print('Confusion Matrix:\n', confusion_matrix(y_test, y_pred))
 print(f'Best params: \n {grid.best_params_}')
-
-# Plot the tree
-plt.figure(figsize=(12, 8))
-plot_tree(best_model, feature_names=diabetes.columns.tolist(), filled=True)
-plt.show()
 
