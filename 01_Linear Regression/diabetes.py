c8b43fffb0f109d604248df61d1968380944eb47
andreanatile <andrea.natile.an@gmail.com>
2024-01-07 10:56:43 +0100

    linear regression sklearn metrics

--- 01_Linear Regression/diabetes.py
+++ 01_Linear Regression/diabetes.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 import numpy as np 
 from Linear_Regression import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 np.random.seed(42)
 
@@ -10,8 +11,8 @@ diabetes=pd.read_csv('.\dataset\diabetes.csv')
 diabetes=diabetes.sample(frac=1).reset_index(drop=True)
 
 # Divide X and y
-X=diabetes.drop('Outcome',axis=1).values
-y=diabetes['Outcome'].values
+X=diabetes.drop('Glucose',axis=1).values
+y=diabetes['Glucose'].values
 
 
 # Split the data in training and test set
@@ -34,3 +35,14 @@ linear_regression=LinearRegression(1e-1,500,X.shape[1])
 
 cost_hisotry,theta_history=linear_regression.fit_full_batch(X_train_std,y_train)
 y_pred=linear_regression.predict(X_test_std)
+
+# Evaluate using regression metrics
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+rmse = np.sqrt(mse)
+r2 = r2_score(y_test, y_pred)
+
+print(f'Mean Absolute Error (MAE): {mae}')
+print(f'Mean Squared Error (MSE): {mse}')
+print(f'Root Mean Squared Error (RMSE): {rmse}')
+print(f'R-squared (R2) Score: {r2}')
